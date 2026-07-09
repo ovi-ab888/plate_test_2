756bf2655b79ab23dfffc5760f55b9c37c10ba6f
ovi-ab888 <oviabdulohab@gmail.com>
2026-07-09 22:44:05 +0600

    Update __init__.py

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -2,28 +2,28 @@
 Algorithms Package - All Plate Ratio Optimizers
 """
 
-from .v1 import V1Optimizer
-from .v2 import V2Optimizer
-from .v3 import V3Optimizer
-from .v4 import V4Optimizer
-from .v5 import V5Optimizer
-from .v6 import V6Optimizer
-from .v7 import V7Optimizer
-from .v8 import V8Optimizer
-from .v11 import V11Optimizer
-from .v12 import V12Optimizer
-from .v15 import V15Optimizer
-from .v16 import V16Optimizer
-from .v17 import V17Optimizer
-from .v18 import V18Optimizer
-from .v19 import V19Optimizer
-from .v20 import V20Optimizer
-from .v21 import V21Optimizer
-from .v22 import V22Optimizer
-from .v24 import V24Optimizer
-from .v25 import V25Optimizer
-from .v26 import V26Optimizer
-from .smart_clustering import SmartClusteringOptimizer
+from algorithms.v1 import V1Optimizer
+from algorithms.v2 import V2Optimizer
+from algorithms.v3 import V3Optimizer
+from algorithms.v4 import V4Optimizer
+from algorithms.v5 import V5Optimizer
+from algorithms.v6 import V6Optimizer
+from algorithms.v7 import V7Optimizer
+from algorithms.v8 import V8Optimizer
+from algorithms.v11 import V11Optimizer
+from algorithms.v12 import V12Optimizer
+from algorithms.v15 import V15Optimizer
+from algorithms.v16 import V16Optimizer
+from algorithms.v17 import V17Optimizer
+from algorithms.v18 import V18Optimizer
+from algorithms.v19 import V19Optimizer
+from algorithms.v20 import V20Optimizer
+from algorithms.v21 import V21Optimizer
+from algorithms.v22 import V22Optimizer
+from algorithms.v24 import V24Optimizer
+from algorithms.v25 import V25Optimizer
+from algorithms.v26 import V26Optimizer
+from algorithms.smart_clustering import SmartClusteringOptimizer
 
 # Registry of all algorithms
 ALGORITHM_REGISTRY = {
