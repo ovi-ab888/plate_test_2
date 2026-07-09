8df1a390a202b7c2a534c4b4b4a1d048a4bc746d
ovi-ab888 <oviabdulohab@gmail.com>
2026-07-09 21:09:26 +0600

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,14 @@
+"""
+Utilities Package - Helper functions for the Plate Ratio System
+"""
 
+from .helpers import (
+    plate_name,
+    create_valid_layout,
+    ensure_demand_met,
+    calculate_waste_percent,
+    build_full_summary
+)
+
+from .pdf_generator import generate_pdf_report
+from .excel_generator import generate_excel_report
